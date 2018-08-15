e47c19f5f611f0e7a1011a00898d81f981d17192
KarenSucely <42396469+KarenSucely@users.noreply.github.com>
2018-08-15 12:47:29 -0500

    Add files via upload

---
+++ Menu.js
@@ -0,0 +1,18 @@
+$(document).ready(main);  //Ejecutar la funcion main
+var contador = 1;
+function main() {
+	$('.Menu_bar').click(function(){
+		if (contador==1) {
+			$('nav').animate({
+				left: '0'
+			});
+			contador=0;
+		}else{
+			contador=1;
+			$('nav').animate({
+				left: '-100%'
+			});
+		}
+	});
+
+};
